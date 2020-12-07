6b7842ff406f6cce8b7a9a537a86a931e5e72391
Mucayit <mucayit.orakci@gmail.com>
2020-12-07 22:42:47 +0100

    Beginning with expenses

---
+++ app/src/main/java/com/example/capstoneproject/model/Outcome.kt
@@ -0,0 +1,25 @@
+package com.example.capstoneproject.model
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalDate
+
+@Entity(tableName = "outcomeTable")
+data class Outcome(
+        @ColumnInfo(name = "title")
+        var title: String,
+
+        @ColumnInfo(name = "description")
+        var description: String,
+
+        @ColumnInfo(name = "amount")
+        var amount: String,
+
+        @ColumnInfo(name = "date")
+        var date: LocalDate,
+
+        @PrimaryKey(autoGenerate = true)
+        @ColumnInfo(name = "id")
+        var id: Long? = null
+)
